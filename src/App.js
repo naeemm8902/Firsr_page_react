c2cc5b1234870aae8b8ec6044207b7a9b11d3611
Naeem Ahmed <113455874+naeemm8902@users.noreply.github.com>
2024-03-20 00:00:49 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import ClassC from './Components/ClassC';
+import Home from './Components/home';
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='main'>
+      <Home/>
+      {/* <ClassC /> */}
+    </div>
+  );
+}
